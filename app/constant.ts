909aad408e956bd4d70cf601c8f4294a6de8db08
NeoGoal <1579523@qq.com>
2023-05-12 14:16:11 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,10 +1,10 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const OWNER = "YYuanYuanAI";
+export const REPO = "圆圆AI实验室(ChatGPT)";
+export const REPO_URL = `http://www.yuanyuandata.com`;
+export const ISSUE_URL = `http://www.yuanyuandata.com`;
+export const UPDATE_URL = `http://www.yuanyuandata.com`;
+export const FETCH_COMMIT_URL = `http://www.yuanyuandata.com`;
+export const FETCH_TAG_URL = `http://www.yuanyuandata.com`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export enum Path {
