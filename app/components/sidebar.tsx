e5b7133342761e55571659f968b5e550c0b8ce54
NeoGoal <1579523@qq.com>
2023-05-12 14:16:08 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -90,9 +90,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>圆圆AI(ChatGPT)实验室</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          建立你自己的的AI助手.
         </div>
         <div className={styles["sidebar-logo"]}>
           <ChatGptIcon />
